4017113850fde90f114bea7ae6a3fb55c978491b
Tasnuva Rahman <tasnuva@codemen.net>
2021-12-23 15:56:48 +0600

    back end for reaction and comments done

--- todo/services/blog/getDownvote.go
+++ todo/services/blog/getDownvote.go
@@ -1,6 +1,29 @@
 package blog
 
+import (
+	"context"
 
-func (s *BlogSvc)GetDownvote(context.Context, *GetDownvoteRequest) (*GetDownvoteResponse, error)  {
-	
+	bpb "github.com/tasnuvatina/grpc-blog/proto/blog"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *BlogSvc)GetDownvote(ctx context.Context,req *bpb.GetDownvoteRequest) (*bpb.GetDownvoteResponse, error)  {
+	id := req.GetBlogID()
+	userId := req.GetUserID()
+
+	_,downvoteId,err := s.core.GetDownvote(context.Background(),id,userId) 
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to your read downvote foe this blog")
+	}
+
+	if downvoteId!=0{
+		return &bpb.GetDownvoteResponse{
+			IsDownvotedId: downvoteId,
+		},nil
+	}else{
+		return &bpb.GetDownvoteResponse{
+			IsDownvotedId: 0,
+		},nil
+	}
 }
--- todo/services/blog/getUpvote.go
+++ todo/services/blog/getUpvote.go
@@ -1,6 +1,31 @@
 package blog
 
+import (
+	"context"
 
-func (s *BlogSvc)GetUpvote(context.Context, *GetUpvoteRequest) (*GetUpvoteResponse, error) {
+	bpb "github.com/tasnuvatina/grpc-blog/proto/blog"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *BlogSvc)GetUpvote(ctx context.Context,req *bpb.GetUpvoteRequest) (*bpb.GetUpvoteResponse, error) {
+	id := req.GetBlogID()
+	userId := req.GetUserID()
+
+	_,upvoteId,err := s.core.GetUpvote(context.Background(),id,userId) 
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to read your upvotes foe this blog")
+	}
+
+	if upvoteId!=0{
+		return &bpb.GetUpvoteResponse{
+			IsUpvotedId: upvoteId,
+		},nil
+	}else{
+		return &bpb.GetUpvoteResponse{
+			IsUpvotedId: 0,
+		},nil
+	}
+
 	
 }
